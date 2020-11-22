bb949b7e3106bab3990993c88bdb3ba7fc1235f9
xxxFilosoFxxx <roman.lashnev@mail.ru>
2020-11-22 06:49:57 +0300

    v1.4.2

--- src/Detection_frame.py
+++ src/Detection_frame.py
@@ -2,7 +2,7 @@ from cv2 import cv2
 from src.Search_speed import Search_speed
 import os
 
-PATH_VIDEO = os.environ.get('VIDEO', 'data_base/пример_дневной.mp4')
+PATH_VIDEO = os.environ.get('VIDEO', 'data_base/Видеонаблюдение.mp4')
 
 
 class Detection_people:
@@ -11,13 +11,14 @@ class Detection_people:
         self.net = cv2.dnn.readNetFromCaffe("MobileNetSSD/MobileNetSSD_deploy.prototxt",
                                             "MobileNetSSD/MobileNetSSD_deploy.caffemodel")
         self.class_name = {15: 'person'}
+        self.percent = 0.2
         self.centroids = Search_speed()
 
     def search_people(self, cols, rows, out, frame):
         for i in range(out.shape[2]):
             confidence = out[0, 0, i, 2]
             class_id = int(out[0, 0, i, 1])
-            if confidence > 0.2 and class_id == 15.0:
+            if confidence > self.percent and class_id in self.class_name:
                 x_left_bottom = int(out[0, 0, i, 3] * cols)
                 y_left_bottom = int(out[0, 0, i, 4] * rows)
                 x_right_top = int(out[0, 0, i, 5] * cols)
@@ -38,27 +39,29 @@ class Detection_people:
                 # Добавление центроидов
                 x_c, y_c = self.centroids.save_centroids(i, x_left_bottom, x_right_top, y_right_top)
 
-                # Обработка скорости
-                # TODO: self.centroids.search_distance(i) -> возвращает линию между центроидами для данного объекта
-                # Почему-то начальное значение пересчета i стоит 6
-                # distance_x, distance_y = self.centroids.search_distance(i)
-                # if distance_x != 0 and distance_y != 0:
-                #     cv2.line(frame, (int(x_c), int(y_c)), (int(distance_x), int(distance_y)), (0, 0, 255), 2)
-
                 # Центроид
                 cv2.circle(frame, (int(x_c), int(y_c)), 5, (0, 0, 255), -1)
 
+                # Обработка скорости
+                # self.centroids.search_distance(i) -> возвращает скорость объекта
+                speed = self.centroids.search_distance(i)
+                if speed != 0:
+                    speed_label = str("%.2f" % speed) + " km/hr"
+                    speed_label_size, base_line = cv2.getTextSize(speed_label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
+
+                    y_left_bottom = max(y_left_bottom, speed_label_size[1])
+                    cv2.rectangle(frame, (x_left_bottom, y_left_bottom - speed_label_size[1]),
+                                  (x_left_bottom + speed_label_size[0], y_left_bottom + base_line),
+                                  (255, 255, 255), cv2.FILLED)
+                    cv2.putText(frame, speed_label, (x_left_bottom, y_left_bottom),
+                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
+
                 # Лэйбл с % точностью определения человека
                 label = self.class_name[class_id] + ": " + str(confidence)
-                label_size, base_line = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.5, 1)
-
-                y_left_bottom = max(y_left_bottom, label_size[1])
-                cv2.rectangle(frame, (x_left_bottom, y_left_bottom - label_size[1]),
-                              (x_left_bottom + label_size[0], y_left_bottom + base_line),
-                              (255, 255, 255), cv2.FILLED)
-                cv2.putText(frame, label, (x_left_bottom, y_left_bottom),
+                cv2.putText(frame, label, (x_left_bottom, y_right_top + 15),
                             cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 0))
                 # print(label)
+                print(speed)
                 # print(x_c, y_c)
 
     def config(self, frame):
@@ -90,7 +93,7 @@ class Detection_people:
     def save_frames(self):
         fourcc = cv2.VideoWriter_fourcc(*'XVID')
         ret, frame = self.cap.read()
-        out_video = cv2.VideoWriter('tests_video_detection/output.avi', fourcc, 20.0, (frame.shape[1], frame.shape[0]))
+        out_video = cv2.VideoWriter('tests_video_detection/output.avi', fourcc, 25.0, (frame.shape[1], frame.shape[0]))
         while self.cap.isOpened():
             ret, frame = self.cap.read()
             if ret:
--- src/Search_speed.py
+++ src/Search_speed.py
@@ -1,22 +1,33 @@
-import numpy as np
+import math
 
 
 class Search_speed:
     def __init__(self):
-        self.centroids = np.array([])
-        self.max = 0
+        self.centroids = dict()
+        self.last_centroids = dict()
 
     def save_centroids(self, i, x_left_bottom, x_right_top, y_right_top) -> (int, int):
-        self.centroids = np.append(self.centroids, [i, [int(x_left_bottom + ((x_right_top - x_left_bottom) / 2)),
-                                                        int(y_right_top)]])
-        self.centroids.shape = (-1, 2)
-        if self.max < i:
-            self.max = i
-        return self.centroids[-1][1][0], self.centroids[-1][1][1]
+        if i not in self.centroids:
+            self.centroids[i] = [int(x_left_bottom + ((x_right_top - x_left_bottom) / 2)), int(y_right_top)]
+            return self.centroids[i][0], self.centroids[i][1]
 
-    def search_distance(self, i) -> (int, int):
-        if i == self.centroids[i - self.max - 1][0] and self.centroids.__len__() > i + 1:
-            return np.sqrt((self.centroids[i - self.max*2 - 2][1][0] - self.centroids[i - self.max - 1][1][0])**2), \
-                   np.sqrt((self.centroids[i - self.max*2 - 2][1][1] - self.centroids[i - self.max - 1][1][1])**2)
+        if i not in self.last_centroids and i in self.centroids:
+            self.last_centroids[i] = [int(x_left_bottom + ((x_right_top - x_left_bottom) / 2)), int(y_right_top)]
+            return self.last_centroids[i][0], self.last_centroids[i][1]
+
+        self.centroids[i] = self.last_centroids[i]
+        self.last_centroids[i] = [int(x_left_bottom + ((x_right_top - x_left_bottom) / 2)), int(y_right_top)]
+
+        return self.last_centroids[i][0], self.last_centroids[i][1]
+
+    def search_distance(self, i):
+        if i in self.centroids and i in self.last_centroids:
+            d_pixels = math.sqrt(math.pow(self.last_centroids[i][0] - self.centroids[i][0], 2) +
+                                 math.pow(self.last_centroids[i][1] - self.centroids[i][1], 2))
+            ppm = 100  # TODO: стоит определять динамически
+            d_meters = d_pixels / ppm
+            fps = 25
+            speed = d_meters * fps * 3.6
+            return speed  # Средняя скорость идущего человека 5-6 км/ч, бег 10-15 км/ч
         else:
-            return 0, 0
+            return 0
